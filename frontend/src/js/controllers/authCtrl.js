7955ea3be52af29a885dcf7b4621afa121f5946f
akashjain993 <akash.jain@iaugmentor.com>
2017-05-27 05:49:55 +0530

    Solved console issue from authCtrl

--- frontend/src/js/controllers/authCtrl.js
+++ frontend/src/js/controllers/authCtrl.js
@@ -11,7 +11,6 @@
 
     function AuthCtrl(utilities, $state, $rootScope) {
         var vm = this;
-
         vm.isRem = false;
         vm.isMail = true;
         vm.userMail = '';
@@ -112,8 +111,8 @@
 
                                 }
 
-                            } catch (error) { /*eslint no-console: ["error", {allow: ["warn", "error"]}]*/
-                                console.error(error);
+                            } catch (error) { 
+                                console.log(error);
                             }
                         }
                         vm.stopLoader();
@@ -157,8 +156,8 @@
                                 if (non_field_errors) {
                                     vm.FormError = response.data.non_field_errors[0];
                                 }
-                            } catch (error) { /*eslint no-console: ["error", {allow: ["warn", "error"]}]*/
-                                console.error(error);
+                            } catch (error) {
+                                console.log(error);
                             }
                         }
                         vm.stopLoader();
